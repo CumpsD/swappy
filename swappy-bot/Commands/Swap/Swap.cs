d74380855b7b9b4ab12fffa0bf8fc4f08a4f71cc
David Cumps <david@cumps.be>
2024-02-01 23:33:11 +0100

    fix: add missing newline

--- swappy-bot/Commands/Swap/Swap.cs
+++ swappy-bot/Commands/Swap/Swap.cs
@@ -170,7 +170,7 @@ namespace SwappyBot.Commands.Swap
                 "Let me start of by mentioning this is a **private thread** and other users **cannot** see this.\n" +
                 "Additionally, I would like to mention this bot is a **community** bot and not an official Chainflip-developed product.\n" +
                 "My source can be reviewed at [GitHub in the `swappy` repository](https://github.com/CumpsD/swappy) to verify all steps.\n" +
-                "You can get in touch with my developers on [Discord](https://discord.gg/wwzZ7a7aQn) in case you have questions." +
+                "You can get in touch with my developers on [Discord](https://discord.gg/wwzZ7a7aQn) in case you have questions.\n" +
                 $"If you reach out for support, be sure to mention reference **{stateId}** to make it easier to help you.",
                 components: buttons,
                 flags: MessageFlags.SuppressEmbeds);
