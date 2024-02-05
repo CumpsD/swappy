26a3a77ec4a963dc3d6ada9c9e42a82592b7fc1d
David Cumps <david@cumps.be>
2024-02-05 20:02:19 +0100

    feat: add newline

--- swappy-bot/Commands/Swap/Swap.cs
+++ swappy-bot/Commands/Swap/Swap.cs
@@ -83,10 +83,10 @@ namespace SwappyBot.Commands.Swap
             //     components: buttons);
 
             await thread.SendMessageAsync(
-                "Hi! It looks like you want to make a swap. I can help you with that.\n" +
+                "Hi! It looks like you want to make a swap. I can help you with that.\n\n" +
                 "Let me start of by mentioning this is a **private thread** and other users **cannot** see this.\n" +
                 "Additionally, I would like to mention this bot is a **community** bot and not an official Chainflip-developed product. " +
-                "By continuing you acknowledge and agree with the service Disclaimer.\n\n" +
+                "By continuing you acknowledge and agree with the Disclaimer (click the button to view).\n\n" +
                 "My source can be reviewed at [GitHub in the `swappy` repository](https://github.com/CumpsD/swappy) to verify all steps.\n" +
                 "You can get in touch with my developers on [Discord](https://discord.gg/wwzZ7a7aQn) in case you have questions.\n" +
                 $"If you reach out for support, be sure to mention reference **{stateId}** to make it easier to help you.",
@@ -578,7 +578,7 @@ namespace SwappyBot.Commands.Swap
                 $"⚠️ **Review your Destination Address and the amounts carefully!** ⚠️\n" +
                 $"The final amount received may vary due to market conditions and network fees.\n" +
                 $"\n" +
-                $"By continuing you acknowledge and agree with the service Disclaimer.\n" +
+                $"By continuing you acknowledge and agree with the Disclaimer.\n" +
                 $"\n" +
                 $"Do you want to proceed with the swap?",
                 components: swapButtons);
