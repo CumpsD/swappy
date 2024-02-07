2add0232434c4e90c233504e8da4ea75ccfaafc8
David Cumps <david@cumps.be>
2024-02-07 22:58:52 +0100

    fix: remove double slash

--- swappy-bot/Commands/Swap/Swap.cs
+++ swappy-bot/Commands/Swap/Swap.cs
@@ -706,7 +706,7 @@ namespace SwappyBot.Commands.Swap
                 $"\n" +
                 $"⚠️ Send **exactly {swapState.Amount} {assetFrom.Ticker}** on the **{assetFrom.Network}** network.\n" +
                 $"\n" +
-                $"🧐 **Verify** the Deposit Address on [Chainflip's official website]({_configuration.ExplorerUrl}/{depositBlock}-{assetFrom.Network}-{depositChannel})!\n" +
+                $"🧐 **Verify** the Deposit Address on [Chainflip's official website]({_configuration.ExplorerUrl}{depositBlock}-{assetFrom.Network}-{depositChannel})!\n" +
                 $"*Keep in mind it can take a few minutes for this page to be accessible, the Chainflip Explorer needs to index the new block first.*\n" +
                 $"\n" +
                 $"ℹ️ In case you have any questions, your swap has reference id **{stateId}**\n" +
@@ -722,7 +722,7 @@ namespace SwappyBot.Commands.Swap
                 "[{StateId}] Provided the deposit instructions to {DepositAddress} -> {ChainflipLink}",
                 stateId,
                 depositAddress,
-                $"{_configuration.ExplorerUrl}/{depositBlock}-{assetFrom.Network}-{depositChannel}");
+                $"{_configuration.ExplorerUrl}{depositBlock}-{assetFrom.Network}-{depositChannel}");
 
             var threadChannel = (IThreadChannel)Context.Channel;
 
