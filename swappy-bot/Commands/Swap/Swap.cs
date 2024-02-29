7cd9a453777d6122a5264fb200508d252e036df5
David Cumps <david@cumps.be>
2024-02-29 19:06:09 +0100

    fix: catch failure to create thread

--- swappy-bot/Commands/Swap/Swap.cs
+++ swappy-bot/Commands/Swap/Swap.cs
@@ -54,48 +54,72 @@ namespace SwappyBot.Commands.Swap
                 addDisclaimer: true,
                 fromDisclaimer: false);
 
-            var channel = (ITextChannel)Context.Channel;
-            var threadName = $"Swap {stateId}";
-            var thread = await channel.CreateThreadAsync(
-                threadName,
-                ThreadType.PrivateThread,
-                ThreadArchiveDuration.OneWeek,
-                invitable: false);
+            try
+            {
+                var channel = (ITextChannel)Context.Channel;
+                var threadName = $"Swap {stateId}";
+                var thread = await channel.CreateThreadAsync(
+                    threadName,
+                    ThreadType.PrivateThread,
+                    ThreadArchiveDuration.OneWeek,
+                    invitable: false);
 
-            await thread.JoinAsync();
+                await thread.JoinAsync();
 
-            await thread.AddUserAsync((IGuildUser)Context.User);
+                await thread.AddUserAsync((IGuildUser)Context.User);
 
-            _logger.LogInformation(
-                "[{StateId}] Created thread {Thread}",
-                stateId,
-                threadName);
+                _logger.LogInformation(
+                    "[{StateId}] Created thread {Thread}",
+                    stateId,
+                    threadName);
 
-            // await thread.SendMessageAsync(
-            //     "Hi! It looks like you want to make a swap. I can help you with that.\n" +
-            //     "Let me start of by mentioning this is a **private thread** and other users **cannot** see this.\n" +
-            //     "\n" +
-            //     "Start by selecting one of the following options:\n" +
-            //     "* **Swap**: Start the swapping wizard.\n" +
-            //     "* **Rates**: Take a look at the current swapping rates.\n" +
-            //     "* **Help**: See some frequently asked questions.\n" +
-            //     "* **About**: Learn more about this bot and get in touch.",
-            //     components: buttons);
+                // await thread.SendMessageAsync(
+                //     "Hi! It looks like you want to make a swap. I can help you with that.\n" +
+                //     "Let me start of by mentioning this is a **private thread** and other users **cannot** see this.\n" +
+                //     "\n" +
+                //     "Start by selecting one of the following options:\n" +
+                //     "* **Swap**: Start the swapping wizard.\n" +
+                //     "* **Rates**: Take a look at the current swapping rates.\n" +
+                //     "* **Help**: See some frequently asked questions.\n" +
+                //     "* **About**: Learn more about this bot and get in touch.",
+                //     components: buttons);
 
-            await thread.SendMessageAsync(
-                "Hi! It looks like you want to make a swap. I can help you with that.\n\n" +
-                "Let me start of by mentioning this is a **private thread** and other users **cannot** see this.\n" +
-                "Additionally, I would like to mention this bot is a **community** bot and **not developed by Chainflip Labs GmbH**. " +
-                "By continuing you acknowledge and agree with the Disclaimer (click the button to view).\n\n" +
-                "My source can be reviewed at [GitHub in the `swappy` repository](https://github.com/CumpsD/swappy) to verify all steps.\n" +
-                "You can get in touch with my developers on [Discord](https://discord.gg/wwzZ7a7aQn) in case you have questions.\n" +
-                $"If you reach out for support, be sure to mention reference **{stateId}** to make it easier to help you.",
-                components: buttons,
-                flags: MessageFlags.SuppressEmbeds);
-
-            await ModifyOriginalResponseAsync(x =>
-                x.Content =
-                    $"Hi! I've created a new **private thread** with you called **Swap {stateId}** to help you with your swap.");
+                await thread.SendMessageAsync(
+                    "Hi! It looks like you want to make a swap. I can help you with that.\n\n" +
+                    "Let me start of by mentioning this is a **private thread** and other users **cannot** see this.\n" +
+                    "Additionally, I would like to mention this bot is a **community** bot and **not developed by Chainflip Labs GmbH**. " +
+                    "By continuing you acknowledge and agree with the Disclaimer (click the button to view).\n\n" +
+                    "My source can be reviewed at [GitHub in the `swappy` repository](https://github.com/CumpsD/swappy) to verify all steps.\n" +
+                    "You can get in touch with my developers on [Discord](https://discord.gg/wwzZ7a7aQn) in case you have questions.\n" +
+                    $"If you reach out for support, be sure to mention reference **{stateId}** to make it easier to help you.",
+                    components: buttons,
+                    flags: MessageFlags.SuppressEmbeds);
+                
+                await ModifyOriginalResponseAsync(x =>
+                    x.Content =
+                        $"Hi! I've created a new **private thread** with you called **Swap {stateId}** to help you with your swap.");
+            }
+            catch (Exception e)
+            {
+                if (e is InteractionException && e.InnerException != null)
+                {
+                    await ModifyOriginalResponseAsync(x =>
+                    {
+                        x.Content =
+                            $"💩 Hi! I've tried creating a new **private thread** with you but received an error: {e.InnerException.Message}.You can try again, or contact us on [Discord](https://discord.gg/wwzZ7a7aQn) for support.";
+                        x.Flags = MessageFlags.SuppressEmbeds;
+                    });
+                }
+                else
+                {
+                    await ModifyOriginalResponseAsync(x =>
+                    {
+                        x.Content =
+                            $"💩 Hi! I've tried creating a new **private thread** with you but received an error: {e.Message}.You can try again, or contact us on [Discord](https://discord.gg/wwzZ7a7aQn) for support.";
+                        x.Flags = MessageFlags.SuppressEmbeds;
+                    });
+                }
+            }
         }
 
         [ComponentInteraction("disclaimer-initial-*")]
